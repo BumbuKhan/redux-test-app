f1a564d050f0ff2f0c060f577dd246d25f0f6ef4
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-09-30 21:44:18 +0400

    A simple redux app has been created

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        {title: 'Book title 1'},
+        {title: 'Book title 2'},
+        {title: 'Book title 3'},
+        {title: 'Book title 4'},
+        {title: 'Book title 5'},
+    ];
+}
